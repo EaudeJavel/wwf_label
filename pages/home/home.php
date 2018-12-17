257c38a66d303439b987ae48ffb725d81368f21b
EaudeJavel <p.plamy6@gmail.com>
2018-12-17 18:00:49 +0100

    finish home body

--- pages/home/home.php
+++ pages/home/home.php
@@ -7,33 +7,80 @@
                 le don d’argent.
             </p>
             <p class="text">
-                L’idée est de créer une mécanique permettant aux entreprises de devenir ambassadrice du <span> respect
+                L’idée est de créer une mécanique permettant aux entreprises de devenir ambassadrice du <span class="bold"> respect
                     de la planète. </span> Celles-ci s'influencent entre elles, et influencent directement leurs
                 consommateurs.
             </p>
             <p class="text">
                 Nous cherchons à repenser le don d’argent par de nouvelles actions. Ces actions se déterminerait par
-                <span>l’éco-responsabilité </span> d’une entreprise dans ses modes de production et de distribution
+                <span class="bold">l’éco-responsabilité </span> d’une entreprise dans ses modes de production et de distribution
             </p>
+            <div class="text bold_text">
+                Dans cette initiative, nous lançons le label Wel, avec 18 marques dans les domaines du prêt-à-porter, de la nourriture et du bien-être, qui se sont déjà ou non engagé à l’éco-responsabilité.</div>
         </div>
+        <div class="discover">Découvrir les marques labelisées WEL</div>
     </div>
     <div class="right_part">
-        <div class="logo"></div>
-        <div class="wel"></div>
+        <div class="wel_container">
+            <div class="logo"></div>
+            <div class="wel"></div>
+        </div>        
         <div class="text_container">
-            <p class="text">
-                Nous souhaitons susciter l’engagement des entreprises et celui de ses consommateurs autrement que par
-                le don d’argent.
-            </p>
-            <p class="text">
-                L’idée est de créer une mécanique permettant aux entreprises de devenir ambassadrice du respect de la
-                planète. Celles-ci s'influencent entre elles, et influencent directement leurs consommateurs.
-            </p>
-            <p class="text">
-                Nous cherchons à repenser le don d’argent par de nouvelles actions. Ces actions se déterminerait par
-                l’éco-responsabilité d’une entreprise dans ses modes de production et de distribution
-            </p>
+            <h2 class="wel_title">
+                Le label garantit à minima :
+            </h2>
+            <div class="text_wrapper">
+                <p class="text">
+                    Nous souhaitons susciter l’engagement des entreprises et celui de ses consommateurs autrement que par
+                    le don d’argent.
+                </p>
+                <p class="text">
+                    L’idée est de créer une mécanique permettant aux entreprises de devenir ambassadrice du respect de la
+                    planète. Celles-ci s'influencent entre elles, et influencent directement leurs consommateurs.
+                </p>
+                <p class="text">
+                    Nous cherchons à repenser le don d’argent par de nouvelles actions. Ces actions se déterminerait par
+                    l’éco-responsabilité d’une entreprise dans ses modes de production et de distribution
+                </p>
+            </div>
         </div>
-
     </div>
 </div>
+
+<div class="second_section">
+        <div class="title">
+            Les marques labelisées
+        </div>
+        <div class="marques_container">
+            <div class="line">
+                <div class="bloc marque"></div>
+                <div class="bloc marque"></div>
+                <div class="bloc marque"></div>
+                <div class="bloc"></div>
+            </div>
+            <div class="line">
+                <div class="bloc marque"></div>
+                <div class="bloc"></div>
+                <div class="bloc marque"></div>
+                <div class="bloc marque"></div>
+            </div>
+            <div class="line">
+                <div class="bloc marque"></div>
+                <div class="bloc marque"></div>
+                <div class="bloc marque"></div>
+                <div class="bloc marque"></div>
+            </div>
+            <div class="line">
+                <div class="bloc marque"></div>
+                <div class="bloc marque"></div>
+                <div class="bloc"></div>
+                <div class="bloc marque"></div>
+            </div>
+            <div class="line">
+                 <div class="bloc marque"></div>
+                <div class="bloc marque"></div>
+                <div class="bloc marque"></div>
+                <div class="bloc marque"></div>  
+            </div>
+        </div>
+    </div>
